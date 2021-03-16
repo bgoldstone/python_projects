7260744fdcd6ffa7623650238112a1d63908f94f
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-03-16 16:03:17 -0400

    Init Comit

---
+++ extensionRenamer.py
@@ -0,0 +1,9 @@
+# a simple program to bulk rename file extensions
+# Author: Ben Goldstone
+import os
+extension = input("Enter New File Extension: ")
+for i in os.listdir():
+    index = i.index(".") + 1
+    if (".py" in i):
+        continue
+    os.rename(i, i[0:index] + extension)
