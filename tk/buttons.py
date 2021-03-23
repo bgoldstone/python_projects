1ec6df2eb9f5fcc88d8e3caedf407b13fd3ef9ef
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-03-23 13:32:37 -0400

    formatting

--- tk/buttons.py
+++ tk/buttons.py
@@ -9,7 +9,7 @@ def main():
 
     # Set Size
     # my_button = Button(root, text="Click Me!", padx=50, pady=50)
-    my_button = Button(root, text="Click Me!", command=my_click, fg="white",bg="red")
+    my_button = Button(root, text="Click Me!", command=my_click, fg="white", bg="red")
     my_button.pack()
 
     root.mainloop()
