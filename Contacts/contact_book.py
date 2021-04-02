b5694b4247e0cf61fad1b1ec09581166f8c39243
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-01 20:04:27 -0400

    Updated contact_book.py

--- Contacts/contact_book.py
+++ Contacts/contact_book.py
@@ -1,3 +1,5 @@
+# contact_book.py
+# Simple not efficient contact book
 import os
 
 
@@ -13,7 +15,13 @@ def main():
             contact_file.write(',')
         contact_file.write('\n')
         contact_file.close()
-
+    contact_file = open('contacts.csv', 'w+')
+    if (contact_file.readline() == ''):
+        for field in fields:
+            contact_file.write(field)
+            contact_file.write(',')
+        contact_file.write('\n')
+        contact_file.close()
     while (choice != 'Q'):
         print("What would you like to do? ")
         print("(V)iew a Contact")
@@ -22,13 +30,14 @@ def main():
         print("(Q)uit")
         choice = input("Please select an Option: ").upper()[0]
         if (choice == 'V'):
-            view(input("Enter the Name you would like to search for "))
+            view(input("Enter the name you would like to search for "))
         if (choice == 'A'):
             add()
+        if (choice == 'R'):
+            remove(input("Enter the name you want to remove "))
 
 
 def view(name):
-    i = True
     with open('contacts.csv', 'r') as contact_file:
         for index, lines in enumerate(contact_file):
             if index == 0:
@@ -39,11 +48,10 @@ def view(name):
                     print("Contact:")
                     for i, field in enumerate(fields):
                         print(f"    {field}: {line[i]}")
-        print("\n\n")
+                    print("\n\n")
 
 
 def add():
-    contact = []
     contact_file = open('contacts.csv', 'a+')
     for field in fields:
         contact_file.write(input(f"Enter the contact's {field} ") + ',')
@@ -51,8 +59,13 @@ def add():
     contact_file.close()
 
 
-def remove():
-    pass
+def remove(name):
+    with open('contacts.csv', 'r') as reader:
+        with open('contacts_tmp.csv', 'w') as writer:
+            for line in reader:
+                if (line.split(',')[0] != name):
+                    writer.write(line)
+    os.replace('contacts_tmp.csv', 'contacts.csv')
 
 
 if __name__ == '__main__':
