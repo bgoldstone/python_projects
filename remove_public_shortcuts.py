c5d74bd104ee48df0244b95084e3638b2bc07a7c
Ben Goldstone <bgoldstone1@gmail.com>
2021-04-13 09:40:22 -0400

    Add files via upload

--- remove_public_shortcuts.py
+++ remove_public_shortcuts.py
@@ -1,10 +1,10 @@
-# remove_shortcuts.py
-# Removes all desktop shortcuts
+# remove_public_shortcuts.py
+# Removes all public desktop shortcuts
 import os
 
 if __name__ == "__main__":
     try:
-        for file in os.listdir(os.path.expanduser('~') + r'\Desktop'):
+        for file in os.listdir(r'C:\Users\Public\Desktop'):
             if file.endswith('.lnk'):
                 print("\n" + file, end="")
                 os.remove(file)
